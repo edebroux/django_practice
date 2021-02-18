722b4e8d423c1f8600eb6db17bf748839fb39a36
Evan De Broux <edebroux@hawk.iit.edu>
2021-02-17 23:06:30 -0600

    Update settings.py

--- simple_django_app/pizzeria/pizzeria/settings.py
+++ simple_django_app/pizzeria/pizzeria/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v0q1m&#r5)ml@g%n&de^wy94g#-ftvp2r8rgcw0sz&s13hinh5'
+SECRET_KEY = '####################################################'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
